018071460457956150fc05ce2ba0a14523ac9a71
SolosoftNL <solosoft.nl@gmail.com>
2015-11-28 23:28:28 +0100

    Init

---
+++ Generics.Repository/ProductRepository/ProductServiceRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Generics.Common;
+using Generics.Common.Interface;
+
+namespace Generics.Repository.ProductRepository
+{
+    public class ProductServiceRepository : IProductRepository
+    {
+        public IEnumerable<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddProduct(Product newProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(int productId, Product updatedProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProducts(IEnumerable<Product> updatedProducts)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Generics.UI/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Documents;
+using Generics.Common;
+using Generics.Common.Factory;
+using Generics.Common.Interface;
+
+namespace Generics.UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NonGenericButton_Click(object sender, RoutedEventArgs e)
+        {
+            PersonListBox.Items.Clear();
+
+            ArrayList people = People.GetNonGenericPeople();
+            people.Add("Hello"); // No compile-time error; strange runtime behavior
+            people.Add(15);      // No compile-time error; strange runtime behavior
+            foreach (var person in people)
+            {
+                PersonListBox.Items.Add(person);
+                //PersonListBox.Items.Add((Person)person); // Runtime error
+            }
+        }
+
+        private void GenericButton_Click(object sender, RoutedEventArgs e)
+        {
+            PersonListBox.Items.Clear();
+
+            List<Person> people = People.GetGenericPeople();
+            //people.Add("Hello"); // Compile-time error
+            //people.Add(15);      // Compile-time error
+            foreach (var person in people)
+                PersonListBox.Items.Add(person);
+        }
+
+        private void RepositoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            //IPersonRepository repo = RepositoryFactory.GetPersonRepository();
+            //var people = repo.GetPeople();
+
+            var repo = Container.Resolve<IRepository<Person, string>>();
+            var people = repo.GetItems();
+
+            foreach (var person in people)
+                PersonListBox.Items.Add(person);
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            PersonListBox.Items.Clear();
+        }
+    }
+}
